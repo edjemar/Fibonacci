bbfe36e21d71ed9a017ad9c7fb6a237f3c0bad14
edjemar <edjemar@gmail.com>
2022-03-27 10:59:18 -0600

    Fibonacci_Recursive

---
+++ Fibonacci/Fibonacci/Fibonacci_Recursive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Recursive
+{
+    internal class Fibonacci_Recursive
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the Length of the Fibonacci Series : ");
+            int number = int.Parse(Console.ReadLine());
+            FibonacciSeries(0, 1, 1, number);
+            Console.ReadKey();
+        }
+        public static void FibonacciSeries(int firstNumber, int secondNumber, int counter, int number)
+        {
+            Console.Write(firstNumber + " ");
+            if (counter < number)
+            {
+                FibonacciSeries(secondNumber, firstNumber + secondNumber, counter + 1, number);
+            }
+        }
+    }
+}
